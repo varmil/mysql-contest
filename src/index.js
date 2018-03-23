d78296cbb21b67df66cd2543de42adb634093279
Satoshi Kuwata <kuwata_satoshi@cygames.co.jp>
2018-03-23 19:23:01 +0900

    課題9まで作成

--- src/index.js
+++ src/index.js
@@ -6,33 +6,153 @@ const { promisify } = require('util')
 const db = require('./database')
 
 exports.e1 = async () => {
-  try {
-    return await db.query('SHOW TABLES')
-  } catch (error) {
-    throw error
-  }
+	try {
+		return await db.query('SHOW TABLES')
+	} catch (error) {
+		throw error
+	}
 }
 
 exports.c3p1 = async () => {
-  try {
-    return await db.query('SELECT emp_id, fname, lname FROM employee ORDER BY lname, fname')
-  } catch (error) {
-    throw error
-  }
+	try {
+		return await db.query('SELECT emp_id, fname, lname FROM employee ORDER BY lname, fname')
+	} catch (error) {
+		throw error
+	}
 }
 
 exports.c3p2 = async () => {
-  try {
-    return await db.query(`SELECT account_id, cust_id, avail_balance FROM account WHERE status = 'ACTIVE' AND avail_balance > 2500`)
-  } catch (error) {
-    throw error
-  }
+	try {
+		return await db.query(`SELECT account_id, cust_id, avail_balance FROM account WHERE status = 'ACTIVE' AND avail_balance > 2500`)
+	} catch (error) {
+		throw error
+	}
 }
 
 exports.c3p3 = async () => {
-  try {
-    return await db.query(`SELECT * FROM account LIMIT 3`)
-  } catch (error) {
-    throw error
-  }
+	try {
+		return await db.query(`SELECT DISTINCT open_emp_id FROM account`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c4p3 = async () => {
+	try {
+		return await db.query(`SELECT account_id, open_date FROM account WHERE open_date BETWEEN '2002-01-01' AND '2002-12-31'`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c4p4 = async () => {
+	try {
+		return await db.query(`SELECT cust_id, lname, fname FROM individual WHERE lname LIKE '_a%e%'`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c5p2 = async () => {
+	try {
+		return await db.query(`SELECT a.account_id, c.fed_id, p.name FROM account a INNER JOIN customer c ON a.cust_id = c.cust_id INNER JOIN product p ON a.product_cd = p.product_cd WHERE c.cust_type_cd = 'I'`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c5p3 = async () => {
+	try {
+		return await db.query(`SELECT e.emp_id, e.fname, e.lname FROM employee e INNER JOIN employee mgr ON e.superior_emp_id = mgr.emp_id WHERE e.dept_id != mgr.dept_id`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c6p2 = async () => {
+	try {
+		return await db.query(`SELECT fname, lname FROM individual UNION SELECT fname, lname FROM employee`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c6p3 = async () => {
+	try {
+		return await db.query(`SELECT fname, lname FROM individual UNION ALL SELECT fname, lname FROM employee ORDER BY lname`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c7p1 = async () => {
+	try {
+		return await db.query(`SELECT SUBSTRING('Please find the substring in this string',17,9)`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c7p2 = async () => {
+	try {
+		return await db.query(`SELECT ABS(-25.76823), SIGN(-25.76823), ROUND(-25.76823, 2)`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c8p1 = async () => {
+	try {
+		return await db.query(`SELECT COUNT(*) from account`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c8p2 = async () => {
+	try {
+		return await db.query(`SELECT cust_id, COUNT(*) FROM account GROUP BY cust_id`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c8p3 = async () => {
+	try {
+		return await db.query(`SELECT cust_id, COUNT(*) FROM account GROUP BY cust_id HAVING COUNT(*) >= 2`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c9p1 = async () => {
+	try {
+		return await db.query(`SELECT account_id, product_cd, cust_id, avail_balance FROM account WHERE product_cd IN (SELECT product_cd FROM product WHERE product_type_cd = 'LOAN')`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c9p2 = async () => {
+	try {
+		return await db.query(`SELECT a.account_id, a.product_cd, a.cust_id, a.avail_balance FROM account a WHERE EXISTS (SELECT 1 FROM product p WHERE p.product_cd = a.product_cd AND p.product_type_cd = 'LOAN')`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c9p3 = async () => {
+	try {
+		return await db.query(`SELECT e.emp_id, e.fname, e.lname, levels.name FROM employee e INNER JOIN (SELECT 'trainee' name, '2004-01-01' start_dt, '2005-12-31' end_dt UNION ALL SELECT 'worker' name, '2002-01-01' start_dt, '2003-12-31' end_dt UNION ALL SELECT 'mentor' name, '2000-01-01' start_dt, '2001-12-31' end_dt) levels ON e.start_date BETWEEN levels.start_dt AND levels.end_dt`)
+	} catch (error) {
+		throw error
+	}
+}
+
+exports.c9p4 = async () => {
+	try {
+		return await db.query(`SELECT e.emp_id, e.fname, e.lname, (SELECT d.name FROM department d WHERE d.dept_id = e.dept_id) dept_name, (SELECT b.name FROM branch b WHERE b.branch_id = e.assigned_branch_id) branch_name FROM employee e`)
+	} catch (error) {
+		throw error
+	}
 }
--- tests/unit/index.js
+++ tests/unit/index.js
@@ -10,46 +10,146 @@ const DSN = `mysql -h${env.MYSQL_HOSTNAME} -uroot -p${env.MYSQL_ROOT_PASSWORD}`
 
 // compare 2 objects
 const assertDeepEqual = (actual, expected) => {
-  // RowDataPacket to Pure Object (hacky solution)
-  const purifyRowDataPackets = (data) => JSON.parse(JSON.stringify(actual))
-  assert.deepEqual(purifyRowDataPackets(actual), expected)
+	// RowDataPacket to Pure Object (hacky solution)
+	const purifyRowDataPackets = (data) => JSON.parse(JSON.stringify(actual))
+	assert.deepEqual(purifyRowDataPackets(actual), expected)
 }
 
 // root-level hook (async)
 beforeEach((done) => {
-  // drop and re-create database && import initial dataset
-  const command = `
+	// drop and re-create database && import initial dataset
+	const command = `
     ${DSN} -e '\
     DROP DATABASE IF EXISTS ${env.MYSQL_DATABASE}; \
     CREATE DATABASE IF NOT EXISTS ${env.MYSQL_DATABASE};' && \
     ${DSN} ${env.MYSQL_DATABASE} < /mocha/tests/_data/learning-sql-example.sql`
 
-  cp.exec(command, (error, stdout, stderr) => {
-    if (error) throw error
-    done()
-  })
+	cp.exec(command, (error, stdout, stderr) => {
+		if (error) throw error
+		done()
+	})
 })
 
 describe('EXAMPLE - 初期状態でこのテストは通ります', () => {
-  it('#e1() すべてのテーブルを取得する。 __1', async () => {
-    const actual = await target.e1()
-    assertDeepEqual(actual, answer.e1)
-  })
+	it('#e1() すべてのテーブルを取得する。 __1', async () => {
+		const actual = await target.e1()
+		assertDeepEqual(actual, answer.e1)
+	})
 })
 
 describe('Chapter 3', () => {
-  it('#c3p1() すべての行員の行員IDと姓名を取得し、性、名の優先順位で並べ替える。 __2', async () => {
-    const actual = await target.c3p1()
-    assertDeepEqual(actual, answer.c3p1)
-  })
+	it('#c3p1() すべての行員の行員IDと姓名を取得し、性、名の優先順位で並べ替える。 __2', async () => {
+		const actual = await target.c3p1()
+		assertDeepEqual(actual, answer.c3p1)
+	})
 
-  it('#c3p2() ステータスがACTIVEであり、残高が2,500ドルを超えるすべての口座について、口座ID、顧客ID、残高を取得する。 __3', async () => {
-    const actual = await target.c3p2()
-    assertDeepEqual(actual, answer.c3p2)
-  })
+	it('#c3p2() ステータスがACTIVEであり、残高が2,500ドルを超えるすべての口座について、口座ID、顧客ID、残高を取得する。 __3', async () => {
+		const actual = await target.c3p2()
+		assertDeepEqual(actual, answer.c3p2)
+	})
 
-  it('#c3p3() --- __4', async () => {
-    const actual = await target.c3p3()
-    assertDeepEqual(actual, answer.c3p3)
-  })
+	it('#c3p3() accountテーブルから口座を開いた行員のIDを取得するためのクエリを作成する。行員ごとに1行のデータが得られるようにする(account.open_emp_id列を使用する)。 __4', async () => {
+		const actual = await target.c3p3()
+		assertDeepEqual(actual, answer.c3p3)
+	})
 })
+
+describe('Chapter 4', () => {
+	it('#c4p3() 2002年に開かれた口座をすべて検索するためのクエリを作成する。 __5', async () => {
+		const actual = await target.c4p3()
+		assertDeepEqual(actual, answer.c4p3)
+	})
+
+	it('#c4p4() 法人顧客ではなく、姓の2文字目に"a"が含まれ、"a"の後ろのどこかに"e"が含まれている顧客をすべて検索するためのクエリを作成する。 __6', async () => {
+		const actual = await target.c4p4()
+		assertDeepEqual(actual, answer.c4p4)
+	})
+})
+
+describe('Chapter 5', () => {
+	it('#c5p2() 法人顧客ではない顧客(customer.cust_type_cd = \'I\')の口座ID、FederalID(customer.fed_id), その口座の商品名(product.name)を取得するためのクエリを作成する  __7', async () => {
+		const actual = await target.c5p2()
+		assertDeepEqual(actual, answer.c5p2)
+	})
+
+	it('#c5p3() 上司が別の部署に所属している行員をすべて検索し、行員のIDと姓名を取得するためのクエリを作成する。 __8', async () => {
+		const actual = await target.c5p3()
+		assertDeepEqual(actual, answer.c5p3)
+	})
+})
+
+describe('Chapter 6', () => {
+	it('#c6p2() 個人顧客全員の姓名と行員全員の生命を検索する複合クエリを作成する。  __9', async () => {
+		const actual = await target.c6p2()
+		assertDeepEqual(actual, answer.c6p2)
+	})
+
+	it('#c6p3() 上記問題(6-2)の結果をlname列で並べ替える。  __10', async () => {
+		const actual = await target.c6p3()
+		assertDeepEqual(actual, answer.c6p3)
+	})
+})
+
+describe('Chapter 7', () => {
+	it('#c7p1() 文字列\"Please find the substring in this string\"の17文字目から25文字目までの文字列を返すクエリを作成する。  __11', async () => {
+		const actual = await target.c7p1()
+		assertDeepEqual(actual, answer.c7p1)
+	})
+
+	it('#c7p2() −25.76823の絶対値と符号(-1, 0, または1)を返すクエリを作成する。また、小数点以下2桁で丸めた値を返す。  __12', async () => {
+		const actual = await target.c7p2()
+		assertDeepEqual(actual, answer.c7p2)
+	})
+})
+
+describe('Chapter 8', () => {
+	it('#c8p1() accountテーブルの行の数をカウントするクエリを作成する。  __13', async () => {
+		const actual = await target.c8p1()
+		assertDeepEqual(actual, answer.c8p1)
+	})
+
+	it('#c8p2() 上記問題(8-1)のクエリを、各顧客が開いている口座の数をカウントするものに書き換える。顧客ごとに、顧客IDと口座の数を表示する。  __14', async () => {
+		const actual = await target.c8p2()
+		assertDeepEqual(actual, answer.c8p2)
+	})
+
+	it('#c8p3() 上記問題(8-2)のクエリを、口座を２つ以上開いている顧客だけを表示するように書き換える。  __15', async () => {
+		const actual = await target.c8p3()
+		assertDeepEqual(actual, answer.c8p3)
+	})
+})
+
+describe('Chapter 9', () => {
+	it('#c9p1() accountテーブルから口座ID、商品コード、顧客ID、口座の残高を取得するためのクエリを作成する。ただし、下記の要件に従うこと。  __16', async () => {
+		/* このクエリでは、フィルタ条件と、productテーブルから全てのローン口座(product.product_type_cd = 'LOAN')を
+		 * 検索するための非相関サブクエリを使用する。
+		 */
+		const actual = await target.c9p1()
+		assertDeepEqual(actual, answer.c9p1)
+	})
+
+	it('#c9p2() 上記問題(9-1)のクエリを、productテーブルへの相関サブクエリを使って同じ結果を得るものに書き換える。  __17', async () => {
+		const actual = await target.c9p2()
+		assertDeepEqual(actual, answer.c9p2)
+	})
+
+	it('#c9p3() 各行員の経験度を表示するために、以下のクエリをemployeeテーブルに結合する。  __18', async () => {
+		/* SELECT 'trainee' name, '2004-01-01' start_dt, '2005-12-31' end_dt 
+		 * UNION ALL
+		 * SELECT 'worker' name, '2002-01-01' start_dt, '2003-12-31' end_dt
+		 * UNION ALL
+		 * SELECT 'mentor' name, '2000-01-01' start_dt, '2001-12-31' end_dt
+		 * 
+		 * サブクエリは"levels"というエイリアスを持ち、行員ID、姓名、経験度(levels.name)が含まれているものとする。
+		 * ヒント:employee.start_date列がどの経験度に属するかを判断する不等価条件に基づいて、結合条件を作成する。
+		 */
+		const actual = await target.c9p3()
+		assertDeepEqual(actual, answer.c9p3)
+	})
+
+	it('#c9p4() employeeテーブルから行員IDと姓名を取得し、行員が配属されている部署と支店の名前を取得するためのクエリを作成する。ただし、テーブルを結合してはならない。  __19', async () => {
+		const actual = await target.c9p4()
+		assertDeepEqual(actual, answer.c9p4)
+	})
+})
+

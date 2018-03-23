de89a6d3905b321c3e1c4a333911f0e3ac864d9e
Your Name <fkmks247@gmail.com>
2018-03-23 14:49:35 +0900

    Implement e1, c3p1, c3p2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+/**
+ * 回答記述
+ * 注）すべて１クエリで結果が返るようにしなさい。
+ */
 const { promisify } = require('util')
 const db = require('./database')
 
-exports.foo = async function () {
-  return await db.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error
-    console.log('The solution is: ', results[0].solution)
-  })
+exports.e1 = async () => {
+  try {
+    return await db.query('SHOW TABLES')
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.c3p1 = async () => {
+  try {
+    return await db.query('SELECT emp_id, fname, lname FROM employee ORDER BY lname, fname')
+  } catch (error) {
+    throw error
+  }
+}
+
+exports.c3p2 = async () => {
+  try {
+    return await db.query(`SELECT account_id, cust_id, avail_balance FROM account WHERE status = 'ACTIVE' AND avail_balance > 2500`)
+  } catch (error) {
+    throw error
+  }
 }
--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,16 +1,22 @@
 const mysql = require('mysql')
 const cp = require('child_process')
-const assert = require('assert')
+const assert = require('assert').strict
 const target = require('../../src/index')
+const answer = require('./answer')
 const env = process.env
 
 // mysql connection string
 const DSN = `mysql -h${env.MYSQL_HOSTNAME} -uroot -p${env.MYSQL_ROOT_PASSWORD}`
 
+// compare 2 objects
+const assertDeepEqual = (actual, expected) => {
+  // RowDataPacket to Pure Object (hacky solution)
+  const purifyRowDataPackets = (data) => JSON.parse(JSON.stringify(actual))
+  assert.deepEqual(purifyRowDataPackets(actual), expected)
+}
+
 // root-level hook (async)
 beforeEach((done) => {
-  // console.log('      --------------- RESET DB ---------------')
-
   // drop and re-create database && import initial dataset
   const command = `
     ${DSN} -e '\
@@ -24,15 +30,21 @@ beforeEach((done) => {
   })
 })
 
-describe('SELECT', () => {
-  describe('CHILD DESC', () => {
-    it('#foobar __1', async () => {
-      await target.foo()
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+describe('EXAMPLE - 初期状態でこのテストは通ります', () => {
+  it('#e1() すべてのテーブルを取得する。 __1', async () => {
+    const actual = await target.e1()
+    assertDeepEqual(actual, answer.e1)
+  })
+})
 
-    it('#barbaz __2', () => {
-      assert.equal([1, 2, 3][1], 2)
-    })
+describe('Chapter 3', () => {
+  it('#c3p1() すべての行員の行員IDと姓名を取得し、性、名の優先順位で並べ替える。 __2', async () => {
+    const actual = await target.c3p1()
+    assertDeepEqual(actual, answer.c3p1)
+  })
+
+  it('#c3p2() ステータスがACTIVEであり、残高が2,500ドルを超えるすべての口座について、口座ID、顧客ID、残高を取得する。 __3', async () => {
+    const actual = await target.c3p2()
+    assertDeepEqual(actual, answer.c3p2)
   })
 })
